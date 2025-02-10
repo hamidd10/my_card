eabf869f18f12a8ea3307212ece2e34e5f7af927
hamidd10 <hamidrahmani3300@gmail.com>
2025-02-10 19:44:00 +0430

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -13,47 +13,74 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10 , 160 , 10 , 0),
-            child: Column(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.red,
-                  radius: 60,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage("images/hamid.jpg"),
+                radius: 60,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Hamid Rahmani',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Pacifico"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    color: Colors.teal[100],
+                    fontSize: 20,
+                    fontFamily: "Source Sans Pro",
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                color: Colors.white,
+                thickness: 1,
+                indent: 80,
+                endIndent: 80,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+93 236 574 587',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source Sans Pro",
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('Hamid Rahmani' , style: TextStyle(color: Colors.white , fontSize: 25),),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('Flutter developer' , style: TextStyle(color: Colors.white , fontSize: 20),),
-                SizedBox(height: 10,),
-                Divider(
-                  color: Colors.white,
-                  thickness: 2,
-                  indent: 10,
-                  endIndent: 10,
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  height: 80,
-                  width: double.infinity,
-                  decoration: BoxDecoration(color: Colors.white , borderRadius: BorderRadius.circular(5)),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 80,
-                  width: double.infinity,
-                  decoration: BoxDecoration(color: Colors.white , borderRadius: BorderRadius.circular(5)),
-                ),
-              ],
-            ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      'hamidrahmani@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: "Source Sans Pro",
+                          fontWeight: FontWeight.bold),
+                    )),
+              ),
+            ],
           ),
         ),
       ),
